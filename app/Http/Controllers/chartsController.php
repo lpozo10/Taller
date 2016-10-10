534622a7286016d164b94cf69d07ebbf885d3714
kvr1191 <kvr1191@gmail.com>
2016-10-09 22:22:10 -0300

    Modelos

--- app/Http/Controllers/chartsController.php
+++ app/Http/Controllers/chartsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Khill\Lavacharts\Lavacharts;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -16,7 +16,28 @@ class chartsController extends Controller
      */
     public function index()
     {
-        //
+        $lava = new Lavacharts; // See note below for Laravel
+
+        $finances = $lava->DataTable();
+
+        $finances->addDateColumn('Year')
+                 ->addNumberColumn('Sales')
+                 ->addNumberColumn('Expenses')
+                 ->setDateTimeFormat('Y')
+                 ->addRow(['2004', 1000, 400])
+                 ->addRow(['2005', 1170, 460])
+                 ->addRow(['2006', 660, 1120])
+                 ->addRow(['2007', 1030, 54]);
+
+        $lava->ColumnChart('Finances', $finances, [
+            'title' => 'Company Performance',
+            'titleTextStyle' => [
+                'color'    => '#eb6b2c',
+                'fontSize' => 14
+            ]
+        ]);
+
+        return view('indexGrafico',compact('lava'));
     }
 
     /**
---
+++ app/Period.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Period extends Model
+{
+    use SoftDeletes;
+    //
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'period';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id','year_init', 'year_end'];
+
+}
